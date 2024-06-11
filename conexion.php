128521961f9a96b2637e4c2f529c0d71837e4852
dianaaa <fuentesvdianap@gmail.com>
2024-06-11 12:56:15 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "tu_usuario";
-$password = "tu_contraseña";
+$username = "id22199860_diana";
+$password = "Hector@123.";
 $dbname = "todo_list";
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
